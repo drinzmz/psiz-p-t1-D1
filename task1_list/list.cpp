b6c9462692e366dc173fc53ae9024c6be1d59197
Najmrodzka <101128573+Najmrodzka@users.noreply.github.com>
2022-03-24 20:24:03 +0100

    Create list.cpp

--- task1_list/list.cpp
+++ task1_list/list.cpp
@@ -6,13 +6,12 @@
 List* initList(void)
 {
 	List* pRoot = (List*)malloc(sizeof(*pRoot));
-	
-	if (pRoot){
-		pRoot->pPlateNum = (char*)"pRoot";
-		time(&(pRoot -> entryTime));
-		pRoot -> pNext = NULL;
-	}
-	return pRoot;
+{
+    pRoot = malloc(sizeof(*pRoot));
+    pRoot->pNumRej = malloc(strlen("Root") + 1u);
+    strcpy(pRoot->pNumRej, "Root");
+    time(&(pRoot->entryTime));
+    pRoot->pNext=NULL;
 }
 
 void deinitList(List* pRoot)
