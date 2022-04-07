8e209360a18d455e3edf07f708b0cc80f7608308
BonkM <83555030+BonkM@users.noreply.github.com>
2022-04-07 20:01:59 +0200

    Add initList function

--- task1_list/list.cpp
+++ task1_list/list.cpp
@@ -1,35 +1,35 @@
 #include "pch.h"
 #include "list.h"
 
-
-
-List* initList(void)
+List *initList(void)
 {
-	List* pRoot = (List*)malloc(sizeof(*pRoot));
-	
+	List *pRoot = (List *)malloc(sizeof(*pRoot));
+	if (NULL == pRoot)
+		return NULL;
+	pRoot->entryTime = 0;
+	pRoot->pNext = 0;
+	pRoot->pPlateNum = 0;
 	return pRoot;
 }
 
-void deinitList(List* pRoot)
+void deinitList(List *pRoot)
 {
 	free(pRoot);
 	pRoot = NULL;
 }
 
-void addCar(List* pRoot, char* plateNum)
+void addCar(List *pRoot, char *plateNum)
 {
-
 }
 
-void removeCar(List* pRoot, char* plateNum)
+void removeCar(List *pRoot, char *plateNum)
 {
-
 }
-void printAll(List* pRoot) {
-
+void printAll(List *pRoot)
+{
 }
 
-int numberOfElements(List* pRoot)
+int numberOfElements(List *pRoot)
 {
 	return 0;
 }
